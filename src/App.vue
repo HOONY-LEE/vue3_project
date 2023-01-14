e1fdb3cb3228b3f17e7f0f0e7e33966c85fd2a71
이성훈 <iseonghun@MacBook-Pro.local>
2023-01-14 19:56:47 +0900

    my first Commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <TopNavbar></TopNavbar>
+  <div class="top_nav_space"></div>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import TopNavbar from './components/TopNavbar.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    TopNavbar,
+}
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+}
+body {
+  box-sizing: content-box;
+  margin: 0px;
+}
+.out1 {
+  outline: 1px dashed green;
+}
+.top_nav_space {
+  width: 100%;
+  height: 50px;
+  background-color: red;
+}
+</style>
---
+++ src/components/TopNavbar.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="top_navbar_container out1">
+        <div class="nav_item_box left_box out1">왼쪽 박스</div>
+        <div class="nav_item_box right_box out1">오른쪽 박스</div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TopNavbar',
+    props: {
+
+    }
+}
+</script>
+
+<style scoped>
+    .top_navbar_container {
+        width: 100%;
+        height: 50px;
+        background-color: gray;
+        position: fixed;
+        display: flex;
+        justify-content: space-between;
+    }
+    .nav_item_box {
+        width: 100px;
+        height: 40px;
+        background-color: white;
+        display: flex;
+        margin-top: 4px;
+    }
+    .top_navbar_container .right_box {
+        width: 30%;
+    }
+    .top_navbar_container .left_box {
+        width: 10%;
+    }
+</style>
+
